2c38e13e407551927e76e4b7a6f91371815145a0
BladeBrosBedwarsBacks1 <recovery.haris@hotmail.com>
2024-08-02 16:27:54 +0100

    Update SetTeams.lua

--- Parts/teams/SetTeams.lua
+++ Parts/teams/SetTeams.lua
@@ -1,7 +1,13 @@
-if userId: 7147426685 then
-    TeamService.setTeam(Yellow)
-elseif userId: 2063174871 then
-    TeamService.setTeam(Pink)
-else
-    TeamService.setTeam(Blue)
+function setTeams()
+    for i, team in ipairs(TeamService.getAllTeams()) do
+        if userId: 7147426685 then
+            TeamService.setTeam(team: Yellow)
+        elseif userId: 2063174871 then
+            TeamService.setTeam(team: Pink)
+        else
+            TeamService.setTeam(team: Blue)
 end
+
+Events.MatchStart(function(event)
+    setTeams()
+end)
